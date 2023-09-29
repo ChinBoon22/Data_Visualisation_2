6f64ac1a7fa5ec783c3f862b81370675c429723c
lowchinboon <clow0021@student.monash.edu>
2023-09-29 17:24:32 +0800

    Update parallel  coordinate chart.

--- Dataset/aggregate_values.py
+++ Dataset/aggregate_values.py
@@ -4,18 +4,18 @@ import pandas as pd
 df = pd.read_csv('Global_Weather_Repository_Cleaned.csv')
 
 # Define the columns you want to aggregate
-columns_to_aggregate = ['gust_mph', 'pressure_mb', 'wind_kph', 'pressure_in', 'visibility_km',
-                        'uv_index', 'air_quality_Ozone', 'air_quality_Nitrogen_dioxide', 'air_quality_Sulphur_dioxide', 'air_quality_PM2.5', 'air_quality_PM10', 'air_quality_us-epa-index', 'air_quality_gb-defra-index']
+columns_to_aggregate = ['gust_mph', 'visibility_km',
+                        'uv_index', 'air_quality_Ozone']
 
 # Group by 'country' and 'continent', and calculate the mean for each specified column
 aggregated_df = df.groupby(['continent'])[
     columns_to_aggregate].mean().reset_index()
 
-aggregated_df[['gust_mph', 'pressure_mb', 'wind_kph', 'pressure_in', 'visibility_km',
-               'uv_index', 'air_quality_Ozone', 'air_quality_Nitrogen_dioxide', 'air_quality_Sulphur_dioxide', 'air_quality_PM2.5', 'air_quality_PM10', 'air_quality_us-epa-index', 'air_quality_gb-defra-index']] = aggregated_df[[
-                   'gust_mph', 'pressure_mb', 'wind_kph', 'pressure_in', 'visibility_km',
-                   'uv_index', 'air_quality_Ozone', 'air_quality_Nitrogen_dioxide', 'air_quality_Sulphur_dioxide', 'air_quality_PM2.5', 'air_quality_PM10', 'air_quality_us-epa-index', 'air_quality_gb-defra-index']].round(2)
+aggregated_df[['gust_mph', 'visibility_km',
+               'uv_index', 'air_quality_Ozone']] = aggregated_df[[
+                   'gust_mph', 'visibility_km',
+                   'uv_index', 'air_quality_Ozone']].round(2)
 
 # Now, filtered_df contains only rows where "wind_direction" is one of the specified values
 # Save the aggregated data to a new CSV file
-aggregated_df.to_csv('aggregated_data_for_stream_graph.csv', index=False)
+aggregated_df.to_csv('aggregated_data_pivot.csv', index=False)
